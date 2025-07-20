69be017c7fc7c24e43589c3e3fdee6eac6fbfd25
nahom zenebe <nahomzenebe123@gmail.com>
2025-07-20 16:09:21 +0300

    fix: error in webpack.config

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   mode: 'development',
   devtool: 'inline-source-map',
